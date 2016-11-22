957079e7fdbb673f7e8a28c1b9ae57926026156f
Colin B Hamilton <cbhsixtysix@gmail.com>
2016-11-22 12:37:11 -0500

    Add tests for constructor, addLatest(), getLatestScheduledTime()

--- src/js/TimedQueue.spec.ts
+++ src/js/TimedQueue.spec.ts
@@ -24,31 +24,9 @@ describe('TimedQueue', function () {
         clock.restore();
     });
 
-    describe('callback', function () {
-        it('gets called immediately if before current time', function () {
-            let spy = sinon.spy();
-            let testQueue = new TimedQueue<number>({
-                callback: function (x: number): void {
-                    spy();
-                }
-            });
-            testQueue.addLatest(-1, millisecondsFrom(-1, new Date()));
-            expect(spy.calledOnce).to.be.true;
-        });
-
-        it('gets called when time is up', function () {
-            let spy = sinon.spy();
-            let testQueue = new TimedQueue<number>({
-                callback: function (x: number): void {
-                    spy();
-                }
-            });
-            testQueue.addLatest(5, millisecondsFrom(5, new Date()));
-
-            clock.tick(4);
-            expect(spy.called).to.be.false;
-            clock.tick(1);
-            expect(spy.calledOnce).to.be.true;
+    describe('constructor', function () {
+        it('should initialize properly', function () {
+            expect(new TimedQueue()).to.be.an.instanceof(TimedQueue);
         });
     });
 
@@ -57,8 +35,40 @@ describe('TimedQueue', function () {
             let testQueue = new TimedQueue<number>();
             testQueue.addLatest(10, millisecondsFrom(10, new Date()));
             let outOfOrderAddition = testQueue.addLatest.bind(testQueue, 10, new Date());
-            expect(outOfOrderAddition).to
+            expect(outOfOrderAddition).to.throw(TimedQueue.OutOfOrderDateError);
         });
+    });
+
+    describe('callback', function () {
+        it('gets called immediately if before current time', function () {
+            let spy = sinon.spy();
+            let testQueue = new TimedQueue<number>({ callback: spy });
+            testQueue.addLatest(-1, millisecondsFrom(-1, new Date()));
+            expect(spy.calledOnce).to.be.true;
+        });
+
+        it('gets called when time is up', function () {
+            let spy = sinon.spy();
+            let testQueue = new TimedQueue<number>({ callback: spy });
+            testQueue.addLatest(5, millisecondsFrom(5, new Date()));
+
+            clock.tick(4);
+            expect(spy.called).to.be.false;
+            clock.tick(1);
+            expect(spy.calledOnce).to.be.true;
+        });
+
+        it('processes multiple items scheduled for the same time');
+    });
+
+    describe('#getLatestScheduledTime', function () {
+        it('should not exist if no dates were added', function () {
+            expect(new TimedQueue().getLatestScheduledTime()).to.not.exist;
+        });
+
+        it('should return the most recently added time');
+
+        it('should no longer exist after last item has been processed');
     });
 });
 
