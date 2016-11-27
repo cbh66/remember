42c18ef9625c9ecb9fba564972b2c85bff61e4a3
Colin B Hamilton <cbhsixtysix@gmail.com>
2016-11-26 22:20:39 -0500

    Add uglify to minimize javascript builds - This is now the default build - Add un-uglified code with debug source maps for development

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
 var tsify = require("tsify");
+var uglify = require("gulp-uglify");
+var sourcemaps = require("gulp-sourcemaps");
+var buffer = require("vinyl-buffer");
 
-gulp.task("default", function () {
+gulp.task("dev", function () {
     return browserify({
         basedir: '.',
         debug: true,
@@ -15,4 +18,22 @@ gulp.task("default", function () {
     .bundle()
     .pipe(source('main.js'))
     .pipe(gulp.dest("build/js"));
+});
+
+gulp.task("default", function () {
+    return browserify({
+	    basedir: '.',
+	    debug: false,
+            entries: ['src/js/controller.ts'],
+            cache: {},
+            packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('main.min.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('build/js'));
 });
