18b22cabf2c1b3ba9b322be45de5cf2b6c2a1b7a
Colin B Hamilton <cbhsixtysix@gmail.com>
2016-11-26 21:03:41 -0500

    Add browserify to combine modules into a single js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var tsify = require("tsify");
 
 gulp.task("default", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("build/js"));
+    return browserify({
+        basedir: '.',
+        debug: true,
+	entries: ['src/js/controller.ts'],
+	cache: {},
+	packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest("build/js"));
 });
