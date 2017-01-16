998d2a66ddbeab8ca6047d5942c75987d7772c92
Colin B Hamilton <cbhsixtysix@gmail.com>
2017-01-15 20:57:05 -0500

    Require the server to connect to database before it starts listening

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
 import * as express from "express";
+import * as mongo from "mongodb";
+var MongoClient = mongo.MongoClient;
 var app = express();
+
+var mongoUrl: string = process.env.MONGODB_URI || 'mongodb://localhost:27017/local';
+console.log(mongoUrl);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -14,6 +19,14 @@ app.get('/build/js/main.js', function(request, response) {
   response.sendFile('./build/js/main.min.js', {root: __dirname});
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+MongoClient.connect(mongoUrl, function (err, db) {
+    if (err) {
+        console.error("Error connecting to database:");
+        console.error(err);
+    } else {
+        console.log("Connected to database");
+        app.listen(app.get('port'), function() {
+          console.log('Node app is running on port', app.get('port'));
+        });
+    }
 });
