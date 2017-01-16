1f72d63883ca39904f320117f2e4959cccd0d06e
Colin B Hamilton <cbhsixtysix@gmail.com>
2017-01-15 22:47:10 -0500

    Add api endpoint for retrieving names in the database - This is just for testing

--- server.ts
+++ server.ts
@@ -6,25 +6,41 @@ var app = express();
 var mongoUrl: string = process.env.MONGODB_URI || 'mongodb://localhost:27017/local';
 console.log(mongoUrl);
 
-app.set('port', (process.env.PORT || 5000));
+function setupAppWithDb(db: mongo.Db) {
+    var names = db.collection('names');
+    app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname));
-app.use("/build", express.static(__dirname + "/build"));
+    app.use(express.static(__dirname));
+    app.use("/build", express.static(__dirname + "/build"));
 
-app.get('/', function(request, response) {
-  response.sendFile('index.html');
-});
+    app.get('/', function(request, response) {
+      response.sendFile('index.html');
+    });
 
-app.get('/build/js/main.js', function(request, response) {
-  response.sendFile('./build/js/main.min.js', {root: __dirname});
-});
+    app.get('/build/js/main.js', function(request, response) {
+      response.sendFile('./build/js/main.min.js', {root: __dirname});
+    });
 
-MongoClient.connect(mongoUrl, function (err, db) {
+    app.get('/api/all', function(request, response) {
+        names.find({}).toArray(function (err, docs) {
+            if (err) {
+                response.status(500).end();
+            } else {
+                response.status(200);
+                response.send(docs);
+            }
+        });
+    });
+}
+
+
+MongoClient.connect(mongoUrl, function (err: mongo.MongoError, db: mongo.Db) {
     if (err) {
         console.error("Error connecting to database:");
         console.error(err);
     } else {
         console.log("Connected to database");
+        setupAppWithDb(db);
         app.listen(app.get('port'), function() {
           console.log('Node app is running on port', app.get('port'));
         });
