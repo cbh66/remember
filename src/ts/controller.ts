118f6451e04f74edeba7834b3ed437a2afd1856c
Colin B Hamilton <cbhsixtysix@gmail.com>
2017-02-14 20:28:27 -0500

    Have frontend pull from db instead of hardcoded data - Still work to be done in continuing to pull from the db

--- src/ts/controller.ts
+++ src/ts/controller.ts
@@ -33,66 +33,7 @@ function updateMemorial(victim: Victim): void {
     verticallyCenter($("#memorial"), $("#memorial-container"));
 }
 
-function addSeconds(date: Date, seconds: number): Date {
-    return new Date(date.getTime() + seconds*1000);
-}
-
-var testContent : Array<Victim> = [
-    {
-        name: "Aaron Burr",
-        event: "Natural Causes",
-        birthYear: 1756,
-        deathYear: 1836,
-        details: "Was a butt",
-        scheduledTime: addSeconds(new Date(), 1)
-    },
-    {
-        name: "Thomas Jefferson",
-        event: "Fiftieth Anniversary of US",
-        birthYear: 1743,
-        deathYear: 1826,
-        details: "Slaveowner and father of US",
-        scheduledTime: addSeconds(new Date(), 6)
-    },
-    {
-        name: "Button Gwinnett",
-        event: "Duel",
-        deathYear: 1777,
-        scheduledTime: addSeconds(new Date(), 11)
-    },
-    {
-        name: "John Hancock",
-        birthYear: 1737,
-        deathYear: 1793,
-        event: "Natural causes",
-        scheduledTime: addSeconds(new Date(), 16)
-    },
-    {
-        name: "Ben Franklin",
-        deathYear: 1790,
-        event: "Natural causes",
-        scheduledTime: addSeconds(new Date(), 21),
-        details: "Loved France; polymath"
-    },
-    {
-        name: "Elbridge Gerry",
-        birthYear: 1744,
-        deathYear: 1814,
-        event: "Died in Office",
-        scheduledTime: addSeconds(new Date(), 26),
-        details: "Only founding father buried in Washington DC"
-    },
-    {
-        name: "Josiah Bartlett",
-        birthYear: 1729,
-        deathYear: 1795,
-        event: "Natural causes",
-        scheduledTime: addSeconds(new Date(), 31)
-    }
-];
-
 let victimList: Queue<Victim> = new Queue<Victim>();
-_.each(testContent, (victim: Victim) => victimList.enqueue(victim));
 
 function updateMemorialLoop(): void {
     console.log("Starting loop");
@@ -121,5 +62,20 @@ function updateMemorialLoop(): void {
 }
 
 $(document).ready(function () {
-    updateMemorialLoop();
+    console.log("trying....");
+    /* TODO: Retry on fail after some time
+     * TODO: Try again:
+     *   - when the queue is empty
+     *   - a few seconds after a failure
+     *   - every few seconds in chunks if queue is below a certain capacity
+     *   - a few minutes after the queue reaches capacity
+     */
+    $.get("api/schedule", function (data: Victim[]) {
+        _.each(data, function (victim: Victim) {
+            victim.scheduledTime = new Date(victim.scheduledTime);
+            victimList.enqueue(victim);
+        });
+        console.log(data);
+        updateMemorialLoop();
+    }, "json");
 });
