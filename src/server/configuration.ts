ef22863c6a26829e6b0be7efe04117d20d7ce4d9
Colin B Hamilton <cbhsixtysix@gmail.com>
2017-02-18 18:17:16 -0500

    Divide configuration type to make more extensible - The required fields have one interface, and have a set of defaults if   they're ignored - Optional fields have their own interface - Options can also be derived based on what's there, and there is a method   for doing exactly that

--- src/server/configuration.ts
+++ src/server/configuration.ts
@@ -1,21 +1,35 @@
 import * as _ from "lodash";
 import * as ConfReader from "./ConfigurationReader";
 
-interface AppConfiguration {
+interface AppConfRequired {
     fadeInTime: number,
     fadeOutTime: number,
     duration: number
 }
 
-let confDefaults: AppConfiguration = {
+let confDefaults: AppConfRequired = {
     fadeInTime: 0,
     fadeOutTime: 0,
     duration: 1000
 }
 
+interface AppConfOptional {
+
+}
+
+interface AppConfDerived {
+
+}
+
+type AppConfiguration = AppConfRequired & AppConfOptional & AppConfDerived;
+
+function deriveProperties(conf: AppConfRequired & AppConfOptional): AppConfiguration {
+    return conf;
+}
+
 function getConfig(pathName: string): AppConfiguration {
     let config: Object = ConfReader.configFromFile(pathName);
-    return _.defaults(config, confDefaults);
+    return deriveProperties(_.defaults(config, confDefaults));
 }
 
 export default getConfig;
