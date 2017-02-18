9841c4d37290031e06f74a96852fe2c1fa3399c0
Colin B Hamilton <cbhsixtysix@gmail.com>
2017-02-18 14:35:10 -0500

    Fix ConfigurationReader types

--- src/server/ConfigurationReader.ts
+++ src/server/ConfigurationReader.ts
@@ -1,12 +1,17 @@
 import * as _ from "lodash";
 import * as fs from "fs";
 
-export function configFromFile(fileName: String, type?: String) {
-    return JSON.parse(fs.readFileSync(fileName, type || 'utf8'));
+export function configFromFile(fileName: string, type?: string): Object {
+    let obj: any = JSON.parse(fs.readFileSync(fileName, type || 'utf8'));
+    if (!_.isObject(obj)) {
+        throw "'"+fileName+"'" + " is not valid JSON (no root object)";
+    } else {
+        return obj;
+    }
 }
 
 // First filenames' props override later ones.  In other words, use later ones for defaults.
-export function configFromFiles(fileNames: String[]) {
+export function configFromFiles(fileNames: string[]): Object {
     return _.defaultsDeep(_.map(fileNames, configFromFile));
 }
 
